c7866526749542c7a405a96318f86da948ff6284
Diwakar <dwkrdahal@gmail.com.com>
2024-06-22 09:42:26 +0545

    express

--- api/index.js
+++ api/index.js
@@ -1,7 +1,24 @@
-import express  from 'express';
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoute from './routes/user.route.js'; 
 
-const app =express();
+dotenv.config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB:', error);
+  });
+
+app.use(express.json());
+
+app.use("/api/user", userRoute);
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000')
+  console.log('Server is running on port 3000! ');
 });
