0dab361d567ff1e8543bbad506d1a4f3cbdf4bee
Diwakar <dwkrdahal@gmail.com.com>
2024-06-22 10:55:17 +0545

    okey

--- api/index.js
+++ api/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRoute from './routes/user.route.js'; 
+import userRouter from './routes/user.route.js'; 
+import authRouter from './routes/auth.route.js'
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URL)
   .then(() => {
@@ -17,7 +20,8 @@ mongoose.connect(process.env.MONGO_URL)
 
 app.use(express.json());
 
-app.use("/api/user", userRoute);
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000! ');
